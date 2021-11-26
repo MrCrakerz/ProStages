0c93ae19753c061e31eee33654058052298a6f55
Your Name <you@example.com>
2021-11-26 10:43:16 +0100

    Fin TP1

--- Prostages/src/Controller/ProstagesController.php
+++ Prostages/src/Controller/ProstagesController.php
@@ -9,12 +9,35 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProstagesController extends AbstractController
 {
     /**
-     * @Route("/prostages", name="prostages")
+     * @Route("/", name="prostages_accueil")
      */
     public function index(): Response
     {
-        return $this->render('prostages/index.html.twig', [
-            'controller_name' => 'ProstagesController',
-        ]);
+        return new Response ('<html > <body > <h1 > Bienvenue sur la page d\'accueil de Prostages </h1 > </ body > </ html >');
     }
+	
+	/**
+	* @Route ("/entreprises" , name =" prostages_entreprises ")
+	*/
+	public function afficherEntreprises () : Response
+	{
+		return new Response ('<html > <body > <h1 > Cette page affichera la liste des entreprises proposant un stage </h1 > </ body > </ html >');
+	}
+	
+	/**
+	* @Route ("/formations" , name =" prostages_formations ")
+	*/
+	public function afficherFormations () : Response
+	{
+		return new Response ('<html > <body > <h1 > Cette page affichera la liste des formations de l\'IUT </h1 > </ body > </ html >');
+	}
+	
+	/**
+	 * @Route ("/stages/{id}" , name =" openclassdut_stages ")
+	 */
+	 public function afficherStages ($id) : Response
+	 {
+		return new Response ('Cette page affichera le descriptif du stage ayant pour identifiant '.$id);
+	 }
 }
+
