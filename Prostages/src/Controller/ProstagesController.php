15f3c8d7b591ef0747eb0630700b6bc9c61720e1
Your Name <you@example.com>
2021-11-30 17:46:22 +0100

    Fin TP 2

--- Prostages/src/Controller/ProstagesController.php
+++ Prostages/src/Controller/ProstagesController.php
@@ -13,31 +13,34 @@ class ProstagesController extends AbstractController
      */
     public function index(): Response
     {
-        return new Response ('<html > <body > <h1 > Bienvenue sur la page d\'accueil de Prostages </h1 > </ body > </ html >');
+        return $this->render('prostages/index.html.twig',['controller_name'=>'Liste Stages']);
+        //return new Response ('<html > <body > <h1 > Bienvenue sur la page d\'accueil de Prostages </h1 > </ body > </ html >');
     }
-	
+
 	/**
 	* @Route ("/entreprises" , name =" prostages_entreprises ")
 	*/
 	public function afficherEntreprises () : Response
 	{
-		return new Response ('<html > <body > <h1 > Cette page affichera la liste des entreprises proposant un stage </h1 > </ body > </ html >');
+      return $this->render('prostages/entreprise.html.twig',['controller_name'=>'Tri par entreprise']);
+    //return new Response ('<html > <body > <h1 > Cette page affichera la liste des entreprises proposant un stage </h1 > </ body > </ html >');
 	}
-	
+
 	/**
 	* @Route ("/formations" , name =" prostages_formations ")
 	*/
 	public function afficherFormations () : Response
 	{
-		return new Response ('<html > <body > <h1 > Cette page affichera la liste des formations de l\'IUT </h1 > </ body > </ html >');
+    return $this->render('prostages/formation.html.twig',['controller_name'=>'Tri par formation']);
+    //return new Response ('<html > <body > <h1 > Cette page affichera la liste des formations de l\'IUT </h1 > </ body > </ html >');
 	}
-	
+
 	/**
 	 * @Route ("/stages/{id}" , name =" openclassdut_stages ")
 	 */
 	 public function afficherStages ($id) : Response
 	 {
-		return new Response ('Cette page affichera le descriptif du stage ayant pour identifiant '.$id);
+     return $this->render('prostages/detailStage.html.twig',['id'=>$id,]);
+		//return new Response ('Cette page affichera le descriptif du stage ayant pour identifiant '.$id);
 	 }
 }
-
