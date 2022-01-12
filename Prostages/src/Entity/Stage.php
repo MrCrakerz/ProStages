47a96a3596158ac6acfd13251ae184cb577d6286
DE LAS HERAS Mathis <mdlheras@iutbayonne.univ-pau.fr>
2022-01-12 16:31:23 +0100

    Résolution du problème de création  d'entités / Créations des 3 entités + migration de ces dernières

---
+++ Prostages/src/Entity/Stage.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StageRepository::class)
+ */
+class Stage
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=250)
+     */
+    private $descMissions;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescMissions(): ?string
+    {
+        return $this->descMissions;
+    }
+
+    public function setDescMissions(string $descMissions): self
+    {
+        $this->descMissions = $descMissions;
+
+        return $this;
+    }
+}
