85cf1e9abbac8eec0503021af75b3ab6346d9f1c
DE LAS HERAS Mathis <mdlheras@iutbayonne.univ-pau.fr>
2022-03-24 22:10:18 +0100

    Acces aux modifications réservé aux ADMIN

--- Prostages/src/Controller/FormationController.php
+++ Prostages/src/Controller/FormationController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/formation")
+ * @Route("/admin")
  */
 class FormationController extends AbstractController
 {
